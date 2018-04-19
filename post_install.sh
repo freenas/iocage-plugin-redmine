509265136e9f51e867c07ee6c0351701d6764bb5
miwi <miwi@ixsystems.com>
2018-04-19 17:57:17 +0800

    - Another try

--- post_install.sh
+++ post_install.sh
@@ -50,8 +50,8 @@ cp /usr/local/www/redmine/config/database.yml.example /usr/local/www/redmine/con
 #fi
 
 sed -i '' "s|username: root|username: ${USER}|g" /usr/local/www/redmine/config/database.yml
-#sed -i '' "s|password: ""|password: ${PASS}|g" /usr/local/www/redmine/config/database.yml
-sed 's/password: ""/password: ${PASS}/g' /usr/local/www/redmine/config/database.yml
+sed -IE '' "s|password: ""|password: ${PASS}|g" /usr/local/www/redmine/config/database.yml
+#sed 's/password: ""/password: ${PASS}/g' /usr/local/www/redmine/config/database.yml
 
 # Precompile the assets
 cd /usr/local/www/redmine
